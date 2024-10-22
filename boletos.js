2bcafbc6749a93687a9e65ea15340f7eb270f6f2
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-10-22 12:42:30 -0300

    Creando archivo boletos.js

---
+++ boletos.js
@@ -0,0 +1,46 @@
+
+// nombre-apellido- edad-númerodeteléfono-socio(valorbooleano)-fechadel
+// partido- hora del partido- número desilla- precio del boleto (con valores
+//  decimales)
+
+
+
+// Pide nombre y apellido
+let nombre = "Zulma";
+console.log("Nombre: " + nombre);
+console.log("Tipo de dato de 'nombre':", typeof(nombre));  
+
+// Pide edad
+let edad = 37;
+console.log("Edad: " + edad);
+console.log("Tipo de dato de 'edad':", typeof(edad));  
+
+// Pide num de teléfono
+let numTelefono = 1123232424;
+console.log("Teléfono: " + numTelefono);
+console.log("Tipo de dato de 'numTelefono':", typeof(numTelefono)); 
+
+// Es socio
+let socio = true;
+console.log("¿Es socio?: " + socio);
+console.log("Tipo de dato de 'socio':", typeof(socio));  
+
+// Fecha del partido
+let fechaPartido = "22/10/2024";
+console.log("Fecha del partido: " + fechaPartido);
+console.log("Tipo de dato de 'fechaPartido':", typeof(fechaPartido));
+
+// Hora del partido
+let horaPartido = "21:00 hs";
+console.log("Hora del partido: " + horaPartido);
+console.log("Tipo de dato de 'horaPartido':", typeof(horaPartido));
+
+// Número de silla
+let numeroSilla = 87;
+console.log("Número de silla: " + numeroSilla);
+console.log("Tipo de dato de 'numeroSilla':", typeof(numeroSilla));
+
+// Precio del boleto
+let precioBoleto = 70000.00;
+console.log("Precio del boleto: " + precioBoleto);
+console.log("Tipo de dato de 'precioBoleto':", typeof(precioBoleto));  
