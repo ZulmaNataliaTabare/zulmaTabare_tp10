7730540dfb2649b148e213b036556646d2600dd4
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-10-22 13:27:32 -0300

    Modifiqué el archivo

---
+++ operaciones.js
@@ -0,0 +1,59 @@
+
+//variables
+
+let num1= 2850;
+
+let num2 = 5525;
+
+let resultadoSuma
+
+let resultadoResta
+
+let resultadoMultiplicacion
+
+let resultadoDivision
+
+let resultadoModulo
+
+
+
+//suma
+resultadoSuma = num1 + num2;
+console.log("El resultado de la Suma es: ", resultadoSuma)
+
+
+//resta
+resultadoResta = num1 - num2;
+console.log("El resultado de la Resta es: ", resultadoResta)
+
+
+//multiplicación
+resultadoMultiplicacion = num1 * num2;
+console.log("El resultado de la Multiplicacion es: ", resultadoMultiplicacion)
+
+
+//división
+resultadoDivision = num1 / num2;
+console.log("El resultado de la Division es: ", resultadoDivision)
+
+
+//módulo
+resultadoModulo = num1 % num2;
+console.log("El resto de la división es: ", resultadoModulo)
+
+
+// ¿Cualdelosvalores esmayor?Sielvaloresmayor,mostraráunvalor
+// boolean true. Delocontrario mostrará false
+// g. ¿Cualdelosvaloresesmenor?Sielvaloresmenormostraráunvalor
+// boolean true.Delocontrario mostrará false
+
+//Mayor
+
+let Mayor = num1 > num2;
+console.log(Mayor);
+
+
+//Menor
+let Menor = num1 < num2;
+console.log(Menor);
+
